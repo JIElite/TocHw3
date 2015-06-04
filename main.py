f411e81aa1eb59755cce17e0481c4da87bc3f728
JIElite <ita3051@gmail.com>
2015-06-05 00:34:37 +0800

    composite the webpage, outlinks together in a list

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import sys
 import json
+import operator
 
 
 if __name__ == "__main__":
-    f = open(sys.argv[1], "r")
+    try:
+        f = open(sys.argv[1], "r")
+        request = int(sys.argv[2])
+    except:
+        print "Error"
+
+    weblist = []
     for line in f.readlines():
         web_encode = json.loads(line)
         #print json.dumps(web_encode, sort_keys=True, indent=4)
@@ -11,8 +18,16 @@ if __name__ == "__main__":
         try:
             weburl = web_encode['Envelope']['WARC-Header-Metadata']['WARC-Target-URI']
             links = web_encode['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Links']
-            print "Web: ", weburl
-            print "Outlink : ", len(links)
-        except:
+            pair = (weburl, len(links))
+            weblist.append(pair)
+        except KeyError:
+            # if there is no 'Links' key in the webpage, we just pass it
             pass
+
+    weblist = sorted(weblist, key = operator.itemgetter(1))
+    weblist.reverse()
+
+    for i in xrange(request):
+        print weblist[i]
+
     f.close()
