8cb68f97542f7b00ee680803f21c76a79153f6f9
JIElite <ita3051@gmail.com>
2015-06-04 23:53:48 +0800

    parse the webpage url for each outlink data

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ if __name__ == "__main__":
     for line in f.readlines():
         web_encode = json.loads(line)
         #print json.dumps(web_encode, sort_keys=True, indent=4)
+
         try:
+            weburl = web_encode['Envelope']['WARC-Header-Metadata']['WARC-Target-URI']
             links = web_encode['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Links']
+            print "Web: ", weburl
             print "Outlink : ", len(links)
         except:
             pass
