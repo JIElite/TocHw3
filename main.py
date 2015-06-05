f9ca2a469133312f3a67d1dd45f1aba47d2f27b7
JIElite <ita3051@gmail.com>
2015-06-05 22:15:05 +0800

    fix sorted function with reverse = True

--- main.py
+++ main.py
@@ -7,25 +7,23 @@ if __name__ == "__main__":
     try:
         f = open(sys.argv[1], "r")
         request = int(sys.argv[2])
-    except:
-        print "Error"
+    except IndexError:
+        print "Please check your input arguments"
 
     weblist = []
     for line in f.readlines():
         web_encode = json.loads(line)
         #print json.dumps(web_encode, sort_keys=True, indent=4)
-
         try:
             weburl = web_encode['Envelope']['WARC-Header-Metadata']['WARC-Target-URI']
             links = web_encode['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Links']
-            pair = (weburl, len(links))
-            weblist.append(pair)
         except KeyError:
             # if there is no 'Links' key in the webpage, we just pass it
-            pass
+            links = ""
+        pair = (weburl, len(links))
+        weblist.append(pair)
 
-    weblist = sorted(weblist, key = operator.itemgetter(1))
-    weblist.reverse()
+    weblist = sorted(weblist, key = operator.itemgetter(1), reverse = True)
 
     for i in xrange(request):
         print weblist[i]
