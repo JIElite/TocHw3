f05bbbe427ff7f38d017290266a93ca12a45c92d
JIElite <ita3051@gmail.com>
2015-06-05 22:39:03 +0800

    fix the output when there are still with same number of outlink

--- main.py
+++ main.py
@@ -2,30 +2,47 @@ import sys
 import json
 import operator
 
+def print_outlink(weblist, top):
+    for i in xrange(top):
+        print weblist[i][0], ":", weblist[i][1]
 
-if __name__ == "__main__":
-    try:
-        f = open(sys.argv[1], "r")
-        request = int(sys.argv[2])
-    except IndexError:
-        print "Please check your input arguments"
+    # Processing the remaining webpage with same outlink number
+    while weblist[top-1][1] == weblist[top][1]:
+        print weblist[top][0], ":", weblist[top][1]
+        top = top + 1
+    return
 
+
+def get_list_outlink(fd):
     weblist = []
-    for line in f.readlines():
+
+    for line in fd.readlines():
         web_encode = json.loads(line)
         #print json.dumps(web_encode, sort_keys=True, indent=4)
         try:
             weburl = web_encode['Envelope']['WARC-Header-Metadata']['WARC-Target-URI']
             links = web_encode['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Links']
         except KeyError:
-            # if there is no 'Links' key in the webpage, we just pass it
+            # If it arise KeyError means there is not ['Link'] element
+            # Because of using len(links) to get the number of outlink
+            # we set links to empty string to get 0
             links = ""
         pair = (weburl, len(links))
         weblist.append(pair)
 
     weblist = sorted(weblist, key = operator.itemgetter(1), reverse = True)
+    return weblist
 
-    for i in xrange(request):
-        print weblist[i]
 
-    f.close()
+
+if __name__ == "__main__":
+    try:
+        fd = open(sys.argv[1], "r")
+        top = int(sys.argv[2])
+    except IndexError:
+        print "Please check your input arguments"
+
+    weblist = get_list_outlink(fd)
+    print_outlink(weblist, top)
+
+    fd.close()
