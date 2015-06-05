b61917f7750f03ee472f49dada052a89bb370a61
JIElite <ita3051@gmail.com>
2015-06-05 23:47:44 +0800

    meet hw3, add file open, top_k error handling

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ import sys
 import json
 import operator
 
+
 def print_outlink(weblist, top):
+    total_data = len(weblist)
+
     for i in xrange(top):
-        print "{0}:{1}".format(weblist[i][0], weblist[i][1])
+        try:
+            print "{0}:{1}".format(weblist[i][0], weblist[i][1])
+        except IndexError:
+            print "Your request is more than total number of data"
+            print "There are total: {0} data".format(total_data)
+            return
 
     # Processing the remaining webpage with same outlink number
-    while weblist[top-1][1] == weblist[top][1]:
-        print "{0}:{1}".format(weblist[top][0], weblist[top][1])
-        top = top + 1
+    if top < total_data:
+        try:
+            while weblist[top-1][1] == weblist[top][1]:
+                print "{0}:{1}".format(weblist[top][0], weblist[top][1])
+                top = top + 1
+        except IndexError:
+            return
     return
 
 
@@ -18,7 +30,7 @@ def get_list_outlink(fd):
 
     for line in fd.readlines():
         web_encode = json.loads(line)
-        #print json.dumps(web_encode, sort_keys=True, indent=4)
+        # print json.dumps(web_encode, sort_keys=True, indent=4)
         try:
             weburl = web_encode['Envelope']['WARC-Header-Metadata']['WARC-Target-URI']
             links = web_encode['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Links']
@@ -30,19 +42,16 @@ def get_list_outlink(fd):
         pair = (weburl, len(links))
         weblist.append(pair)
 
-    weblist = sorted(weblist, key = operator.itemgetter(1), reverse = True)
+    weblist = sorted(weblist, key=operator.itemgetter(1), reverse=True)
     return weblist
 
 
-
 if __name__ == "__main__":
-    try:
-        fd = open(sys.argv[1], "r")
-        top = int(sys.argv[2])
-    except IndexError:
-        print "Please check your input arguments"
-
-    weblist = get_list_outlink(fd)
-    print_outlink(weblist, top)
-
-    fd.close()
+    with open(sys.argv[1], "r") as fd:
+        try:
+            top_k = int(sys.argv[2])
+        except IndexError as index_err:
+            print "There is no input top_k"
+            sys.exit(0)
+        weblist = get_list_outlink(fd)
+        print_outlink(weblist, top_k)
